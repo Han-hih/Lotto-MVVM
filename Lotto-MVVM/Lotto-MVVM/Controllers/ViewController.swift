bb707d3dde189dcfff5e7d9546d90ed2c76a1cd4
한 <hih458@naver.com>
2023-09-18 00:49:09 +0900

    add 메인화면 오토레이아웃 설정

--- Lotto-MVVM/Lotto-MVVM/Controllers/ViewController.swift
+++ Lotto-MVVM/Lotto-MVVM/Controllers/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let sessionTextField = {
+        let tf = UITextField()
+        tf.placeholder = "회차를 입력해주세요"
+        tf.backgroundColor = .lightGray
+        tf.layer.cornerRadius = 8
+        tf.clipsToBounds = true
+        return tf
+    }()
+    
+    let sessionButton = {
+        let bt = UIButton()
+        bt.backgroundColor = .red
+        bt.setTitle("당첨 정보 확인하러 가기", for: .normal)
+        bt.layer.cornerRadius = 8
+        bt.clipsToBounds = true
+        bt.addTarget(self, action: #selector(sessionButtonTapped), for: .touchUpInside)
+        
+        return bt
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        [sessionTextField, sessionButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        setAutoLayout()
     }
-
+    
+    func setAutoLayout() {
+        NSLayoutConstraint.activate([
+            //회차정보 입력 텍스트필드
+            sessionTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            sessionTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            sessionTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            sessionTextField.heightAnchor.constraint(equalToConstant: 40),
+            //당첨 정보 확인 버튼
+            sessionButton.topAnchor.constraint(equalTo: sessionTextField.bottomAnchor, constant: 50),
+            sessionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    
+    @objc func sessionButtonTapped(_ sender: UIButton) {
+        
+    }
 
 }
 
