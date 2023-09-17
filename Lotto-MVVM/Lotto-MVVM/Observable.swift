cf27379005adff2dd65bad1b8068e85378747dd7
한 <hih458@naver.com>
2023-09-18 01:07:21 +0900

    add Observable 클래스 생성

---
+++ Lotto-MVVM/Lotto-MVVM/Observable.swift
@@ -0,0 +1,31 @@
+//
+//  Observable.swift
+//  Lotto-MVVM
+//
+//  Created by 황인호 on 2023/09/18.
+//
+
+import Foundation
+
+class Observable<T> {
+    private var listener: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+            print(value, "----")
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ sample: @escaping (T) -> Void) {
+        sample(value)
+        print(value, "0000000")
+        listener = sample
+    }
+    
+    
+}
