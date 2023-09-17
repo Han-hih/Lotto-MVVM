d89e8c3ebdabb0808abb08c18dc84f3241e0ac1f
한 <hih458@naver.com>
2023-09-18 00:12:42 +0900

    add Lotto Codable 생성

---
+++ Lotto-MVVM/Lotto-MVVM/Models/Lotto.swift
@@ -0,0 +1,20 @@
+//
+//  Lotto.swift
+//  Lotto-MVVM
+//
+//  Created by 황인호 on 2023/09/17.
+//
+
+import Foundation
+
+struct Lotto: Codable {
+    let drwNoDate: String
+    let firstWinamnt: Int
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+    let bnusNo: Int
+}
