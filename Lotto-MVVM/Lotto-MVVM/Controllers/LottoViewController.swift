8e11028a07821d34de2d8b40be978ab3b4d9452e
한 <hih458@naver.com>
2023-09-18 14:01:32 +0900

    chore 파일 생성 및 파일 이동
    -ViewModels 그룹 생성
    -Observable -> Models로 이동
    -뷰컨트롤러 이름 LottoViewController로 바꿈

--- Lotto-MVVM/Lotto-MVVM/Controllers/LottoViewController.swift
+++ Lotto-MVVM/Lotto-MVVM/Controllers/LottoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LottoViewController: UIViewController {
 
     let apiManager = APIRequest()
     
---
+++ Lotto-MVVM/Lotto-MVVM/VIewModels/LottoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LottoViewModel.swift
+//  Lotto-MVVM
+//
+//  Created by 황인호 on 2023/09/18.
+//
+
+import Foundation
